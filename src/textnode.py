b8898c0402282c2d0ec7537cc0ca48755c886b66
KebbaPhilip <kebbaphilip@gmail.com>
2026-09-03 16:33:41 +0000

    modif textnode obj

--- src/textnode.py
+++ src/textnode.py
@@ -11,12 +11,35 @@ class TextType(Enum):
     IMAGE = "![alt text](url)"
 
 
-class TextNode:
+from enum import Enum
 
-    def __init__(self, text, type, url=None):
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    CODE = "code"
+    LINK = "link"
+    IMAGE = "image"
+
+
+class TextNode:
+    def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
         self.text = text
-        self.text_type = type
+        self.text_type = text_type
         self.url = url
+
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, TextNode):
+            return False
+        return (
+            self.text_type == other.text_type
+            and self.text == other.text
+            and self.url == other.url
+        )
+
+    def __repr__(self) -> str:
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
     def __eq__(self, other):
         if not isinstance(other, TextNode):
