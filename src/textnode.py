73f3b8225d191ace475413ca5fbe07850279551a
KebbaPhilip <kebbaphilip@gmail.com>
2026-09-04 12:17:23 +0000

    text to html

--- src/textnode.py
+++ src/textnode.py
@@ -1,18 +1,5 @@
 from enum import Enum
-from typing import override
-
-
-class TextType(Enum):
-    TEXT = "plain"
-    BOLD = "**Bold text**"
-    ITALIC = "_Italic text_"
-    CODE = "`Code text`"
-    LINKS = "[anchor text](url)"
-    IMAGE = "![alt text](url)"
-
-
-from enum import Enum
-
+from htmlnode import LeafNode
 
 class TextType(Enum):
     TEXT = "text"
@@ -53,3 +40,22 @@ class TextNode:
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+def text_node_to_html_node(text_node: TextNode) -> LeafNode:
+    if text_node.text_type == TextType.TEXT:
+        return LeafNode(None, text_node.text)
+    if text_node.text_type == TextType.BOLD:
+        return LeafNode("b", text_node.text)
+    if text_node.text_type == TextType.ITALIC:
+        return LeafNode("i", text_node.text)
+    if text_node.text_type == TextType.CODE:
+        return LeafNode("code", text_node.text)
+    if text_node.text_type == TextType.LINK:
+        if text_node.url is None:
+            raise ValueError("invalid URL")
+        return LeafNode("a", text_node.text, {"href": text_node.url})
+    if text_node.text_type == TextType.IMAGE:
+        if text_node.url is None:
+            raise ValueError("invalid URL")
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+    raise ValueError(f"invalid text type: {text_node.text_type}")
