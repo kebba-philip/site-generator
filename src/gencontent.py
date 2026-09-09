583e22432b0a74cdb5b57f1f07c1d09a683552ae
KebbaPhilip <kebbaphilip@gmail.com>
2026-09-09 19:02:21 +0000

    Fix page routing for deployment

--- src/gencontent.py
+++ src/gencontent.py
@@ -9,13 +9,25 @@ def generate_pages_recursive(
 ) -> None:
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
-        if os.path.isfile(from_path):
-            dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path, basepath)
-        else:
-            generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
+        if os.path.isfile(from_path):
+            dest_path = os.path.join(
+                dest_dir_path,
+                filename
+            )
+
+            if filename.endswith(".md"):
+                dest_path = Path(dest_path).with_suffix(".html")
+                generate_page(from_path, template_path, dest_path, basepath)
+
+        else:
+            dest_path = os.path.join(dest_dir_path, filename)
+            generate_pages_recursive(
+                from_path,
+                template_path,
+                dest_path,
+                basepath
+            )
 
 def generate_page(
     from_path: str, template_path: str, dest_path: str | Path, basepath: str
