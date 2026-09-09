b9b5dc5ebced31d302aaea1a8b19cac14443beef
KebbaPhilip <kebbaphilip@gmail.com>
2026-09-09 17:47:59 +0000

    rename new directory and remove public

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from copystatic import copy_files_recursive
 from gencontent import generate_page
 
 dir_path_static = "./static"
-dir_path_public = "./docs"
+dir_path_public = "./docs"  ## new dir
 dir_path_content = "./content"
 template_path = "./template.html"
 
